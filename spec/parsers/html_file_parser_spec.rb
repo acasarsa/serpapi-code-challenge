89757da2dc05984707068d3755e01ce5a4c55a69
Andrew Casarsa <acasarsa@gmail.com>
2024-08-02 16:54:20 -0400

    Images do not match as expected - js issue? * parsing test uncovered that images do not match in parsing as expected from personal inspection of live site with dev tools * research suggests gifs are used for placeholders during lazy loading...so javascript may be required to get accurate image data * also noticed some scripts in the html that could be contributing but commenting out did not solve. but does also point towards needing javascript

--- spec/parsers/html_file_parser_spec.rb
+++ spec/parsers/html_file_parser_spec.rb
@@ -2,10 +2,16 @@
 
 require 'spec_helper'
 require 'nokogiri'
+require 'pry'
+require 'json'
 require 'parsers/html_file_parser'
 
 RSpec.describe GoogleCarouselExtractor::Parsers::HtmlFileParser do
   let(:file_path) { './spec/fixtures/van-gogh-paintings.html' }
+  let(:expected_result_json) do
+    file_content = File.read('./spec/fixtures/expected-array.json')
+    JSON.parse(file_content)
+  end
 
   subject(:parser) { described_class.new(file_path) }
 
@@ -13,6 +19,14 @@ RSpec.describe GoogleCarouselExtractor::Parsers::HtmlFileParser do
     it 'parses the HTML file and returns a Nokogiri::HTML::Document' do
       content = parser.ready_html_content
       expect(content).to be_a(Nokogiri::HTML::Document)
+    end
+
+    it 'should contain expected images', :focus do
+      content = parser.ready_html_content
+      parsed_carousel_images = content.at_css('g-scrolling-carousel').css('a').css('img')
+      first_image_src_value = parsed_carousel_images[0].attribute_nodes[2].value
+      # binding.pry
+      expect(first_image_src_value).to eq(expected_result_json['artworks'][0]['image'])
     end
   end
 end
