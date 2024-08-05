4c00d159915d4a24cad36388cda74e91740a33e4
Andrew Casarsa <acasarsa@gmail.com>
2024-08-05 10:49:50 -0400

    Add lots of tests for extractor, edge handling, and updates to extractor class * add private method testing * extract method testing with different files * image processing * non visible image handling * add option to extract specific indices from parsed doc * handle differences in html structure for non-visible images between van gogh and dali. dali having a view all link.   ^ opted to go with custom data attribute over checking for view all text or role. * added logger warn in place of regular warn for fallback method involving class name * cleaned up some comments and binging.prys

--- lib/google_carousel_extractor/carousel_extractor.rb
+++ lib/google_carousel_extractor/carousel_extractor.rb
@@ -3,18 +3,24 @@
 module GoogleCarouselExtractor
   # Processes the parsed data and extracts it into desired structure
   class CarouselExtractor
-    def initialize(parser)
-      @doc = parser.parse_html
+    def initialize(parser, html_source)
+      @doc = parser.parse_html(html_source)
     end
 
-    def extract
+    def extract(indices = nil)
       artworks = []
-
       a_tags = @doc.at_css('g-scrolling-carousel').css('a')
+
+      a_tags = a_tags.select.with_index { |_, i| indices.include?(i) } if indices # If indices are provided, filter the a_tags
+
       a_tags.each do |a_tag|
+        data_ti_attribute = a_tag.attribute_nodes.find { |attr| attr.name == 'data-ti' }&.value
+
+        break if data_ti_attribute == 'ArtistToArtworks'
+
+        title = extract_title(a_tag)
         link = build_link(a_tag)
         image = extract_image_src(a_tag)
-        title = extract_title(a_tag)
         date = extract_date(a_tag)
         artworks << construct_artwork_object(name: title, date: date, link: link, image: image)
       end
@@ -58,7 +64,7 @@ module GoogleCarouselExtractor
         return date_text if valid_year?(date_text)
       end
 
-      warn "Warning: Using fallback method to extract date. Check if 'ellip' class has changed or is missing."
+      GoogleCarouselExtractor.logger.warn("Using fallback method to extract date. Check if 'ellip' class has changed or is missing.")
       fallback_date_text = a_tag.text&.strip&.gsub(/\s+/, ' ')&.split(' ')&.last
       valid_year?(date_text) ? fallback_date_text : nil
     end
--- spec/carousel_extractor_spec.rb
+++ spec/carousel_extractor_spec.rb
@@ -5,81 +5,134 @@ require 'json'
 require 'parsers/html_file_parser'
 require 'carousel_extractor'
 
-require 'pry'
-
 RSpec.describe GoogleCarouselExtractor::CarouselExtractor do
-  let(:file_path) { './spec/fixtures/van-gogh-paintings.html' }
-  let(:file_path2) { './spec/fixtures/salvador-dali-paintings' }
-  let(:parser) { GoogleCarouselExtractor::Parsers::HtmlFileParser.new(file_path) }
-  let(:extractor) { GoogleCarouselExtractor::CarouselExtractor.new(parser) }
-  let(:expected_artworks) do
-    file_content = File.read('./spec/fixtures/expected-array.json')
-    JSON.parse(file_content)['artworks']
-  end
-  let(:expected_artworks_with_images) do
-    file_content = File.read('./spec/fixtures/expected-array-with-images.json')
-    JSON.parse(file_content)['artworks']
-  end
-  let!(:extracted_artworks) { extractor.extract }
-
-  subject(:extractor) { described_class.new(parser) }
+  let(:parser) { double('Parser', parse_html: parsed_html) }
 
   describe '#extract' do
-    it 'processes image data properly' do
-      expected_artworks_with_images.each_with_index do |artwork, index|
-        extracted_image = process_base64_image(extracted_artworks[index]['image'])
-        expected_image = process_base64_image(artwork['image'])
+    let(:parsed_html) do
+      Nokogiri::HTML::DocumentFragment.parse(<<~HTML)
+        <g-scrolling-carousel>
+          <a href="/link1" title="Artwork 1 (1888)">
+            <img src="https://example.com/image1.jpg" alt="Artwork 1" />
+            <div class="ellip">1888</div>
+          </a>
+          <a href="/link2" title="Artwork 2">
+            <img src="https://example.com/image2.jpg" alt="Artwork 2" />
+          </a>
+          <a href="/link3" title="Artwork 3" data-src="placeholder_image.jpg">
+            <div class="ellip">1931</div>
+          </a>
+          <a href="/link4" title="Artwork 4 (1905)">
+            <img src="https://example.com/image4.jpg" alt="Artwork 4" />
+          <div>1905</div> </a>
+          <a data-ti="ArtistToArtworks">...</a>
+        </g-scrolling-carousel>
+      HTML
+    end
 
-        expect(extracted_image).to eq(expected_image)
+    it 'extracts artwork information correctly' do
+      extractor = described_class.new(parser, 'dummy_source')
+
+      expect(extractor.extract).to eq([
+                                        { 'name' => 'Artwork 1',
+                                          'link' => 'https://www.google.com/link1',
+                                          'image' => 'https://example.com/image1.jpg',
+                                          'extensions' => ['1888'] },
+                                        { 'name' => 'Artwork 2',
+                                          'link' => 'https://www.google.com/link2',
+                                          'image' => 'https://example.com/image2.jpg' },
+                                        { 'name' => 'Artwork 3',
+                                          'link' => 'https://www.google.com/link3',
+                                          'image' => nil,
+                                          'extensions' => ['1931'] },
+                                        { 'name' => 'Artwork 4', 'link' => 'https://www.google.com/link4', 'image' => 'https://example.com/image4.jpg' }
+                                      ])
+    end
+
+    context 'private method testing' do
+      let(:extractor) { described_class.new(parser, 'dummy_source') }
+      let(:a_tag) { parsed_html.at_css('a') }
+
+      it 'builds the link correctly' do
+        allow(extractor).to receive(:build_link).and_call_original
+        expect(extractor.send(:build_link, a_tag)).to eq('https://www.google.com/link1')
       end
+
+      it 'extracts the title correctly' do
+        allow(extractor).to receive(:extract_title).and_call_original
+        expect(extractor.send(:extract_title, a_tag)).to eq('Artwork 1')
+      end
+
+      it 'extracts the date correctly' do
+        allow(extractor).to receive(:extract_date).and_call_original
+        expect(extractor.send(:extract_date, a_tag)).to eq('1888')
+      end
+
+      it 'extracts the image src correctly' do
+        allow(extractor).to receive(:extract_image_src).and_call_original
+        expect(extractor.send(:extract_image_src, a_tag)).to eq('https://example.com/image1.jpg')
+      end
+    end
+  end
+  describe '#extract (with real HTML data)' do
+    let(:van_gogh_html) { './spec/fixtures/van-gogh-paintings.html' }
+    let(:dali_html) { './spec/fixtures/salvador-dali-paintings.html' }
+
+    let(:expected_van_gogh_artworks) do
+      JSON.parse(File.read('./spec/fixtures/expected-array.json'))['artworks']
+    end
+
+    let(:expected_dali_artworks) do
+      JSON.parse(File.read('./spec/fixtures/expected-dali-array.json'))
+    end
+
+    it 'extracts Van Gogh artwork information correctly' do
+      parser = GoogleCarouselExtractor::Parsers::HtmlFileParser.new
+      extractor = described_class.new(parser, van_gogh_html)
+
+      extracted_artworks = extractor.extract
+
+      compare_artworks_handling_nil_image_cases(extracted_artworks, expected_van_gogh_artworks)
+    end
+
+    it 'extracts Dali artwork information correctly' do
+      parser = GoogleCarouselExtractor::Parsers::HtmlFileParser.new
+      extractor = described_class.new(parser, dali_html)
+
+      extracted_artworks = extractor.extract
+
+      compare_artworks_handling_nil_image_cases(extracted_artworks, expected_dali_artworks)
+    end
+
+    it 'properly extracts name when date is nil and does not add extensions key' do
+      parser = GoogleCarouselExtractor::Parsers::HtmlFileParser.new
+      extractor = described_class.new(parser, van_gogh_html)
+
+      extracted_artwork = extractor.extract([2]).first
+      expect(extracted_artwork).not_to have_key('extensions')
+      expect(extracted_artwork['name']).to eq expected_van_gogh_artworks[2]['name']
     end
 
     context 'processing placeholder images as nil' do
       it "processes soon-to-be-visible placeholder images with custom data attribute: 'data-src' as nil" do
-        extracted_image1 = extracted_artworks[8]['image']
+        parser = GoogleCarouselExtractor::Parsers::HtmlFileParser.new
+        extractor = described_class.new(parser, van_gogh_html)
 
-        expect(extracted_image1).to be_nil
-        expect(extracted_image1).to eq(expected_artworks[8]['image'])
+        nil_image = extractor.extract([8]).first['image']
+
+        expect(nil_image).to be_nil
+        expect(nil_image).to eq(expected_van_gogh_artworks[8]['image'])
       end
 
       it 'processes blank gif placeholder images as nil' do
-        extracted_image2 = extracted_artworks[10]['image']
+        parser = GoogleCarouselExtractor::Parsers::HtmlFileParser.new
+        extractor = described_class.new(parser, van_gogh_html)
 
-        expect(extracted_image2).to be_nil
-        expect(extracted_image2).to eq(expected_artworks[10]['image'])
-      end
-    end
+        nil_image = extractor.extract([10]).first['image']
 
-    it 'properly extracts name and date when date is nil' do
-      sunflower_name = expected_artworks[2]['name']
-      ext_sunflower_name = extracted_artworks[2]['name']
-      sunflower_date = expected_artworks[2]['extensions']
-      ext_sunflower_date = extracted_artworks[2]['extensions']
-
-      expect(ext_sunflower_name).to eq(sunflower_name)
-      expect(ext_sunflower_date).to eq(sunflower_date)
-    end
-
-    it 'extracts data from html as expected' do
-      expect(extracted_artworks.length).to eq expected_artworks.length
-
-      expected_artworks.each_with_index do |artwork, index|
-        expected_name = artwork['name']
-        expected_link = artwork['link']
-        expected_date = artwork['extensions']
-
-        extracted_name = extracted_artworks[index]['name']
-        extracted_link = extracted_artworks[index]['link']
-        extracted_date = extracted_artworks[index]['extensions']
-
-        expect(extracted_name).to eq(expected_name)
-        expect(extracted_link).to eq(expected_link)
-        expect(extracted_date).to eq(expected_date)
+        expect(nil_image).to be_nil
+        expect(nil_image).to eq(expected_van_gogh_artworks[10]['image'])
       end
     end
   end
 end
-
-# TODO: test with other url
-# test it with a different url
-# create the readme
