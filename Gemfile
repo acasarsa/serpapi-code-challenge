f82c08453371589a726dc5337dfa3a1c99b8e1be
Andrew Casarsa <acasarsa@gmail.com>
2024-08-05 11:30:25 -0400

    added demo method and updated logger console formating, add some gems to Gemfile

--- Gemfile
+++ Gemfile
@@ -3,11 +3,12 @@
 source 'https://rubygems.org'
 
 gem 'nokogiri'
-gem 'open-uri'
 gem 'rspec'
 gem 'watir'
 
 group :development do
   gem 'pry'
+  gem 'mini_magick'
+  gem 'base64'
   gem 'rubocop', require: false
 end
--- lib/google_carousel_extractor.rb
+++ lib/google_carousel_extractor.rb
@@ -1,17 +1,48 @@
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'open-uri'
 require 'logger'
+require 'json'
 $LOAD_PATH.unshift(File.expand_path('../lib/google_carousel_extractor', __dir__))
 
 require 'parsers/html_file_parser'
 require 'services/browser_service'
 require 'carousel_extractor'
+require_relative '../spec/support/extraction_comparison_helpers'
 
-# Main project run file
+# Sets up a demo and logger for app
 module GoogleCarouselExtractor
   def self.logger
-    @logger ||= Logger.new($stdout)
+    @logger ||= Logger.new($stdout).tap do |log|
+      # Simple console formatter
+      log.formatter = proc do |severity, _datetime, _progname, msg|
+        "#{severity}: #{msg}\n"
+      end
+    end
+  end
+
+  def self.demo
+    # ImageHelpers for demo use to process base64 image strings.
+    # Use: process_base64_image(base64_string)
+    #
+    # Example: exp_image = process_base64_image(van_gogh_expected_array[0]['image'])
+    #
+    extend ImageHelpers
+    # demo extraction from HTML file with van gogh paintings
+    file_path = File.expand_path('../spec/fixtures/van-gogh-paintings.html', __dir__)
+    van_gogh_expected_array = JSON.parse(File.read('spec/fixtures/expected-array.json'))['artworks']
+    parser = GoogleCarouselExtractor::Parsers::HtmlFileParser.new
+    extractor = GoogleCarouselExtractor::CarouselExtractor.new(parser, file_path)
+    Pry.start(binding)
+    van_gogh_paintings = extractor.extract
+    pp van_gogh_paintings[0] # size 51
+
+    # demo extraction from HTML file with salvador dali paintings
+    file_path2 = File.expand_path('../spec/fixtures/salvador-dali-paintings.html', __dir__)
+    dali_expected_array = JSON.parse(File.read('spec/fixtures/expected-dali-array.json'))
+    parser2 = GoogleCarouselExtractor::Parsers::HtmlFileParser.new
+    extractor2 = GoogleCarouselExtractor::CarouselExtractor.new(parser2, file_path2)
+    Pry.start(binding)
+    salvador_dali_paintings = extractor2.extract
+    pp salvador_dali_paintings[0] # size 11
   end
 end
